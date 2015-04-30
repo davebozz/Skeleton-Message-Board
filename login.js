6ab11af8d0d8bc4535d05bd10387029bc4e0554e
Bozentka David <dbozentka@linus-public.highpoint.edu>
2015-04-29 21:11:18 -0400

    Added login file.js & login.php, Changed the dialog box CSS & Modified HTML

--- login.js
+++ login.js
@@ -1,15 +1,69 @@
 function LoginForm(form)
 {
+
 	//Check to see if UserName was entered appropriate
-	if(form.Username.value==""){
-		$('.EnterError').show();
+	if(form.UserName.value==""){
+		var errorText = "Please fill in your UserName.";
+		$('#UserError').text(errorText);
+	}
+	else{
+		var errorText ="";
+		$('#UserError').text(errorText);
 	}
 
 	//Checks to see if UserPassword was filled out
 	if(form.UserPassword.value==""){
-		console.log("HI");
-		$('.EnterError').show();
+		var errorText = "Please fill in your Password.";
+		$('#PassError').text(errorText);
+	}
+	else{
+		var errorText ="";
+		$('#PassError').text(errorText);
 	}
 
+	//=-If The UserName & Password are filled,
+	//=-Send both of them to the database
+	if(form.UserPassword.value!="" && form.UserName.value !="")
+	{
+		$.post(
+			'login.php',
+			{"User_Name":UserName.value, "User_Pass":UserPassword.value},
+			function(resData){
+				jsonResData = $.parseJSON(resData);
 
+				//Comment out post testing.
+				console.log(jsonResData);
+				console.log(jsonResData.type);
+
+				if(jsonResData.type == "No_User"){
+					var errorText = "No Username, Please Rework or Register Above.";
+					$('#UserError').text(errorText);
+					form.UserName.value="";
+					form.UserName.focus();
+					form.UserPassword.value="";
+					form.UserPassword.focus();
+				}
+
+				if(jsonResData.type == "Bad_Pass"){
+					var errorText = "Incorrect Password, try again.";
+					$('#UserError').text(errorText);
+					form.UserPassword.value="";
+					form.UserPassword.focus();
+				}
+
+				if(jsonResData.type == "Logged_In"){
+					var First_Name = jsonResData.First_Name;
+					var Last_Name = jsonResData.Last_Name;
+
+					//Remove after debug
+					console.log(First_Name);
+					console.log(Last_Name);
+
+					//Close dialog box
+					$('#Login').dialog('close');
+				}
+
+		});
+
+	}
 }
--- login.php
+++ login.php
@@ -1,3 +1,81 @@
 <?php
+	session_start();
+	//Begin the session-->
+	require("../include/utility.php");
+	$dbconn = connectToDB();
 
+	/*
+		-Turn this into a function after working.
+		-Clean it up: Could be much more fluid.
+	*/
+	//Check to see if both the Username & Password are set
+	if(isset($_POST["User_Name"]) && isset($_POST["User_Pass"]))
+	{
+		$User_Name = cleanInput($_POST['User_Name']);
+		$User_Pass = cleanInput($_POST['User_Pass']);
+
+		//Protect against MySQL Injection
+		$User_Name = $dbconn->real_escape_string($User_Name);
+		$User_Pass = $dbconn->real_escape_string($User_Pass);
+
+		//Remove after debug
+		logMsg($User_Name);
+		logMsg($User_Pass);
+
+		$query = "SELECT * FROM Users WHERE Username = '$User_Name';";
+
+		//Remove after debug
+		logMsg($query);
+
+		if((!$result = $dbconn->query($query)))
+		{
+			logMsg("Bad Query");
+		}
+		
+		if($result->num_rows <= 0)
+		{
+			$message = array("type"=>"No_User");
+			$encode = json_encode($message);
+			//Remove after debug
+			logMsg($encode);
+			echo $encode;
+		} else {
+			
+			//Get my row and compare
+			$myrow = $result->fetch_array();
+
+			if($myrow['Password'] == $User_Pass)
+			{
+				/*
+					==-If Password & Username are both correct
+						begin the session and send it throughout 
+						each php script
+					==- Grab FName/LName/ID/
+				*/
+				//Assign First/Last Names & Return Via JSON
+				$First_Name = $myrow['First_Name'];
+				$Last_Name = $myrow['Last_Name'];
+
+				//Begin Session Variables
+				$_SESSION['User_Name']=$User_Name; //Initalizing the UserName
+				$_SESSION['User_First']=$First_Name; //Initializing the First Name
+				$_SESSION['User_Last']=$Last_Name; //Intializing the Last Name
+
+				$message = array("type"=>"Logged_In","First_Name"=>$First_Name,
+									"Last_Name"=>$Last_Name);
+				$encode = json_encode($message);
+				logMsg($encode);
+				echo $encode;
+
+			} else {
+				$message = array("type"=>"Bad_Pass");
+				$encode = json_encode($message);
+				//Remove after debug;
+				logMsg($encode);
+				echo $encode;
+			}
+		}
+
+	}
+disconnectDB($dbconn);
 ?>
